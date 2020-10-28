9dbdb614e2b92b081f0d4030688c06b7ac211064
livekumon <65915023+livekumon@users.noreply.github.com>
2020-10-27 23:19:47 -0400

    updated the services production URL

--- src/Services/config.js
+++ src/Services/config.js
@@ -5,7 +5,7 @@ exports.services = {
   //   changePassword: "",
   // },
   prod: {
-    main: "https://livekumon-services.herokuapp.com",
+    main: "https://live-kumon-services.herokuapp.com",
     // main: "https://livesloka.azurewebsites.net",
     login: "/login",
     changePassword: "/ChangePassword",
