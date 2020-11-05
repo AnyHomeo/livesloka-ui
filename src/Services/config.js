6e15eab827e24dd2d3a44dda64885ebb7a196264
Ronin <iamraviprakash1@gmail.com>
2020-11-05 23:44:52 +0530

    UI fix

--- src/Services/config.js
+++ src/Services/config.js
@@ -1,7 +1,7 @@
 exports.services = {
   prod: {
-    // main: "https://live-kumon-services.herokuapp.com",
-    main: "http://localhost:5000",
+    main: "https://live-kumon-services.herokuapp.com",
+    // main: "http://localhost:5000",
     login: "/login",
     changePassword: "/ChangePassword",
 
