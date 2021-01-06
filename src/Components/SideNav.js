258480985a17238c6b020099f437bd09ad80e53e
E.Karthik <kartheekenumarthi@gmail.com>
2021-01-06 18:18:09 +0530

    fix changes

--- src/Components/SideNav.js
+++ src/Components/SideNav.js
@@ -138,8 +138,8 @@ export default function PersistentDrawerLeft({ children }) {
             {theme.direction === "ltr" ? (
               <ChevronLeftIcon />
             ) : (
-              <ChevronRightIcon />
-            )}
+                <ChevronRightIcon />
+              )}
           </IconButton>
         </div>
         <Divider />
@@ -150,6 +150,7 @@ export default function PersistentDrawerLeft({ children }) {
             { title: "Attendance", link: "/attendance" },
             { title: "Teacher Timetable", link: "/scheduler" },
             { title: "Meeting Schedule", link: "/meeting-scheduler" },
+            { title: "Add Teacher Details", link: "/teacherDetails" },
             { title: "Reset Password", link: "/reset/password" },
           ].map((navItem, index) => (
             <Link
