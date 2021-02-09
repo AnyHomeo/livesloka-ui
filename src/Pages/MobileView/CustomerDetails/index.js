76e407e4ece85f0154b165c212b0de010e1c93b7
Kamal Vasamsetti <srisaikamal2000@gmail.com>
2021-02-09 18:38:42 +0530

    Filtering based on seaerch

--- src/Pages/MobileView/CustomerDetails/index.js
+++ src/Pages/MobileView/CustomerDetails/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getAllCustomerDetails } from "../../../Services/Services";
-import { Paper, Grid, Card } from "@material-ui/core/";
+import { Paper, Grid, Card, TextField } from "@material-ui/core/";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 
@@ -20,14 +20,34 @@ const CustomerDetails = () => {
   const classes = useStyles();
 
   const [customersData, setCustomersData] = useState();
+  const [filteredData, setFilteredData] = useState();
+  const [searchKeyword, setSearchKeyword] = useState("");
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     fetchCustomerDetails();
   }, []);
 
   const fetchCustomerDetails = async () => {
+    setLoading(true);
     const data = await getAllCustomerDetails();
-    setCustomersData(data && data.data);
-    console.log(data);
+    setCustomersData(data && data.data.result);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    filterData();
+  }, [searchKeyword]);
+
+  const filterData = () => {
+    function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    let value = capitalizeFirstLetter(searchKeyword);
+    let regex = new RegExp(`^${value}`, `i`);
+    const sortedArr =
+      customersData && customersData.filter((x) => regex.test(x.firstName));
+
+    setFilteredData(sortedArr);
   };
 
   return (
@@ -54,6 +74,15 @@ const CustomerDetails = () => {
           Add new
         </Card>
       </Link>
+      <TextField
+        fullWidth
+        style={{ marginBottom: "20px" }}
+        label="Search"
+        variant="outlined"
+        onChange={(e) => setSearchKeyword(e.target.value)}
+        value={searchKeyword}
+      />
+
       <Grid
         container
         spacing={2}
@@ -61,32 +90,61 @@ const CustomerDetails = () => {
         justify="center"
         alignItems="center"
       >
-        {customersData &&
-          customersData.result.map((data) => (
-            <Grid item xs={4}>
-              <Link
-                to={{
-                  pathname: "/customer-data-info",
-                  state: { data },
-                }}
-              >
-                <Card
-                  style={{
-                    width: 110,
-                    height: 70,
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    textAlign: "center",
-                    backgroundColor: "#2ecc71",
-                    color: "white",
+        {loading
+          ? ""
+          : searchKeyword
+          ? filteredData &&
+            filteredData.map((data) => (
+              <Grid item xs={4}>
+                <Link
+                  to={{
+                    pathname: "/customer-data-info",
+                    state: { data },
                   }}
                 >
-                  {data.firstName}
-                </Card>
-              </Link>
-            </Grid>
-          ))}
+                  <Card
+                    style={{
+                      width: 110,
+                      height: 70,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      textAlign: "center",
+                      backgroundColor: "#2ecc71",
+                      color: "white",
+                    }}
+                  >
+                    {data.firstName}
+                  </Card>
+                </Link>
+              </Grid>
+            ))
+          : customersData &&
+            customersData.map((data) => (
+              <Grid item xs={4}>
+                <Link
+                  to={{
+                    pathname: "/customer-data-info",
+                    state: { data },
+                  }}
+                >
+                  <Card
+                    style={{
+                      width: 110,
+                      height: 70,
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      textAlign: "center",
+                      backgroundColor: "#2ecc71",
+                      color: "white",
+                    }}
+                  >
+                    {data.firstName}
+                  </Card>
+                </Link>
+              </Grid>
+            ))}
       </Grid>
     </div>
   );
